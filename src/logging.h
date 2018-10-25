8a54e5fa20542437317a33d6aa50df406da8669e
fauer4effect <kyle.fauer@gmail.com>
2018-10-25 07:20:36 -0400

    fix headerfile

--- src/logging.h
+++ src/logging.h
@@ -11,4 +11,4 @@ log_level designates the level of messages to print
     2 = info, all messages with type=1 will print messages with type=0 will not print
     0 = silent, no messages will print
 */
-void logger(int type, int log_level, char *msg, ...);
+void logger(int type, int log_level, int process_id, char *msg, ...);
